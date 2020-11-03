2f1ec57cc522c3addce1b11a9cecb756a694260d
jahazielBH <dmotodroide@gmail.com>
2020-11-03 12:58:39 -0600

    add exp function - refactoring #15

--- src/app/exp/exp.ts
+++ src/app/exp/exp.ts
@@ -1,4 +1,7 @@
-export function exp(oper1, oper2) {  
+export function exp(base, exp) {  
+    // refactoring
 
-    return Math.pow(oper1, oper2);
+    let pow = 0;
+    pow = Math.pow(base, exp);
+    return pow;
 }
