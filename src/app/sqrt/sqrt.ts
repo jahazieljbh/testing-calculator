9bb1cb489a8b328c159c21d41c23be41dd8906f5
jahazielBH <dmotodroide@gmail.com>
2020-11-09 10:01:42 -0600

    add sqrt - refactoring #17

--- src/app/sqrt/sqrt.ts
+++ src/app/sqrt/sqrt.ts
@@ -1,3 +1,6 @@
-export function sqrt(oper1) {  
-    return Math.sqrt(oper1);
+export function sqrt(oper1) {
+    //refactoring
+    let sqrt = 0;
+    sqrt = Math.sqrt(oper1);
+    return sqrt;
  }
